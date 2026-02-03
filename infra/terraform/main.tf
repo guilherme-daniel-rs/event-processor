e005dbeefc351602d3e9822c5dceeac33772054b
guilherme-daniel-rs <guidanirs@gmail.com>
2026-02-03 09:02:42 -0300

    chore: create dynamo db on terraform

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -5,12 +5,18 @@ provider "aws" {
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
+
+  endpoints {
+    dynamodb = "http://localhost:4566"
+    sqs      = "http://localhost:4566"
+  }
 }
 
 locals {
-  main_queue_name      = "events-main"
-  dlq_queue_name       = "events-dlq"
-  max_receive_count    = 5
+  main_queue_name   = "events-main"
+  dlq_queue_name    = "events-dlq"
+  max_receive_count = 5
+  events_table_name = "events"
 }
 
 resource "aws_sqs_queue" "dlq" {
@@ -29,6 +35,37 @@ resource "aws_sqs_queue" "main" {
   receive_wait_time_seconds = 20
 }
 
+resource "aws_dynamodb_table" "events" {
+  name         = local.events_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "event_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "tenant_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "client_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "occurred_at"
+    type = "S"
+  }
+}
+
 output "main_queue_url" {
   value = aws_sqs_queue.main.url
 }
@@ -39,4 +76,12 @@ output "dlq_queue_url" {
 
 output "dlq_queue_arn" {
   value = aws_sqs_queue.dlq.arn
+}
+
+output "events_table_name" {
+  value = aws_dynamodb_table.events.name
+}
+
+output "events_table_arn" {
+  value = aws_dynamodb_table.events.arn
 }
