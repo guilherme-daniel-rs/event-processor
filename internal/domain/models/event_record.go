457b922a3031efd72b828021fc6d27f287c39514
guilherme-daniel-rs <guidanirs@gmail.com>
2026-02-04 09:38:25 -0300

    fix: error to put item on dynamo

--- internal/domain/models/event_record.go
+++ internal/domain/models/event_record.go
@@ -1,14 +1,14 @@
 package models
 
 type EventRecord struct {
-	ID            string
-	EventID       string
-	TenantID      string
-	ClientID      string
-	SchemaVersion string
-	OccurredAt    string
-	Status        string
-	Body          string
+	ID            string `dynamodbav:"id"`
+	EventID       string `dynamodbav:"event_id"`
+	TenantID      string `dynamodbav:"tenant_id"`
+	ClientID      string `dynamodbav:"client_id"`
+	SchemaVersion string `dynamodbav:"schema_version"`
+	OccurredAt    string `dynamodbav:"occurred_at"`
+	Status        string `dynamodbav:"status"`
+	Body          string `dynamodbav:"body"`
 }
 
 func (e EventRecord) TableName() *string {
